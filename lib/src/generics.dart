4c725600dd2ba962c0b92e6064782a127881b9fc
maprohu <maprohu@gmail.com>
2023-09-02 16:11:52 +0100

    assert generic function

--- lib/src/generics.dart
+++ lib/src/generics.dart
@@ -4,6 +4,9 @@ extension MhuGenericsX<T> on T {
 
 typedef GenericFunction1<T> = R Function<R>(R Function<TT extends T>() fn);
 
-GenericFunction1<B> genericFunction1<B, T extends B>() => <R>(fn) => fn<T>();
+GenericFunction1<B> genericFunction1<B, T extends B>() {
+  assert(B != T);
+  return <R>(fn) => fn<T>();
+}
 
 GenericFunction1<T> genericFunction1Flat<T>() => genericFunction1<T, T>();
